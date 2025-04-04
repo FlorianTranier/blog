12634189d7d3f4da874412f00771e275bda8e640
Florian Tranier <tranierflorian@pm.me>
2025-04-04 18:54:39 +0200

    feat: seo

--- content.config.ts
+++ content.config.ts
@@ -1,14 +1,17 @@
 import { defineContentConfig, defineCollection, z } from "@nuxt/content";
+import { asSeoCollection } from "@nuxtjs/seo/content";
 
 export default defineContentConfig({
   collections: {
-    blog: defineCollection({
-      type: "page",
-      source: "blog/**.md",
-      schema: z.object({
-        title: z.string(),
-        date: z.date(),
-      }),
-    }),
+    blog: defineCollection(
+      asSeoCollection({
+        type: "page",
+        source: "blog/**.md",
+        schema: z.object({
+          title: z.string(),
+          date: z.date(),
+        }),
+      })
+    ),
   },
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxtjs/seo", "@nuxt/content"],
   devtools: { enabled: true },
   nitro: {
     preset: "cloudflare_pages",
